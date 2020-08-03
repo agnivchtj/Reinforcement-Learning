5ad7fe226fb4eca7c14368daf05976978daa62e2
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2020-08-03 20:06:56 +0200

    Add project files to repo

---
+++ src/Action.java
@@ -0,0 +1,8 @@
+
+public class Action {
+	public String id;
+	public Action(String id) {
+		//the action id is simply the identifier of the type of action, such as up down left etc...
+		this.id = new String(id);
+	}
+}
